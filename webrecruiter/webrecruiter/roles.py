1bc1030c397f256353e6da94f1db2f724039cf37
fahnateecha <blue_fy@outlook.com>
2018-10-10 23:49:49 +0700

    try permissiom

--- webrecruiter/webrecruiter/roles.py
+++ webrecruiter/webrecruiter/roles.py
@@ -2,10 +2,13 @@ from rolepermissions.roles import AbstractUserRole
 
 class Hr(AbstractUserRole):
     available_permissions = {
-        'create_medical_record': True,
+        'Can add candidate rank': True,
+        'Can change candidate rank': True,
+        'Can delete candidate rank': True,
+        'Can view candidate rank': True,
     }
 
-class Engineer(AbstractUserRole):
-    available_permissions = {
-        'edit_patient_file': True,
-    }
+# class Engineer(AbstractUserRole):
+#     available_permissions = {
+#         'edit_patient_file': True,
+#     }
