5119ae7726c21f6cf6fcf95f33aa9e958124f5b3
nateecha <nateecha18@outlook.com>
2018-10-11 15:46:18 +0700

    Rank Candidate & Permission Hr

--- webrecruiter/rank/models.py
+++ webrecruiter/rank/models.py
@@ -1,10 +1,19 @@
 from django.db import models
+from jobapply.models import CandidateBasic
+from account.models import Profile
 
 # Create your models here.
 class CandidateRank(models.Model):
     """docstring forCandidate_History_Education."""
-    candidate = models.CharField(max_length=50, blank=True, null=True)
-    count = models.CharField(max_length=50, blank=True, null=True)
+    candidate = models.OneToOneField(CandidateBasic, on_delete=models.SET_NULL, null=True)
+    # candidate = models.CharField(max_length=50, blank=True, null=True)
+    count = models.IntegerField(blank=True, null=True,default=0)
+    owner = models.ManyToManyField(Profile)
 
-    def __int__(self):
-        return self.candidate
+
+    def __str__(self):
+        return self.candidate.id_number
+
+    def get_candidate_owner(self):
+        print("_____________________",self.owner.all())
+        return self.owner.all()
--- webrecruiter/rank/views.py
+++ webrecruiter/rank/views.py
@@ -8,14 +8,31 @@ from . import views
 
 from django.views.generic import View
 from rank.models import CandidateRank
+from jobapply.models import CandidateBasic
+from candidate_cart.models import OrderItem,Order
+from rolepermissions.decorators import has_role_decorator
 
 
-
-# Create your views here.
+@has_role_decorator('hr')
 def index(request):
-    rank = CandidateRank.objects.all()
+    CandidateRank.objects.all().delete()
+    orders = Order.objects.all()
+    for order in orders:
+        items = order.items.all()
+        owner = order.owner
+        print(owner)
+        for item in items:
+            print(item)
+            candidate = CandidateBasic.objects.filter(id_number=item).first()
+            candidate_rank = CandidateRank.objects.get_or_create(candidate = candidate)[0]
+            count = candidate_rank.count + 1
+            candidate_rank.owner.add(owner)
+            print(owner,item,"=","Counted",count)
+            CandidateRank.objects.filter(candidate=candidate).update(count=count)
+            print("ALL OWNER",candidate_rank.owner.all())
+    ranks = CandidateRank.objects.all().order_by('-count')
     context = {
-        'rank' : rank
+        'ranks' : ranks,
     }
     template = loader.get_template("rank.html")
     return HttpResponse(template.render(context, request))
